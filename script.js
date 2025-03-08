b941633a48a6c9a5b784f021ce49eb578f9dd648
Kaiqian-Z17 <kaiqian0917@gmail.com>
2025-03-08 21:10:29 +0000

    HI BOB!!

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 function fix() {
-    //Write your code here!
+   let coconut = document.getElementById("coconut");
+
+   coconut.remove()
+
+   let coast = document.getElementById("coast");
+
+   coast.remove()
+
+   let watermelon = document.getElementById("watermelon");
+
+   watermelon.remove()
+
+   let sunflowers = document.getElementById("sunflowers");
+
+   sunflowers.remove()
+
+   let tree = document.createElement("img");
+
+   tree.id = "tree";
+   tree.src = "to-add/christmasTree.jpg";
+   
+   let photos = document.getElementById("photos");
+
+   photos.appendChild(tree);
+
+   let snowmen = document.createElement("img");
+
+   snowmen.id = "tree";
+   snowmen.src = "to-add/snowmen.jpg";
+
+   let w = document.getElementById("photos");
+
+   photos.appendChild(snowmen);
+
+   let stockings = document.createElement("img");
+
+   stockings.id = "tree";
+   stockings.src = "to-add/stockings.jpg";
+
+   let b = document.getElementById("photos");
+
+   photos.appendChild(stockings);
+
 }
